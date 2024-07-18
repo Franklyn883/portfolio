8c98eb1f616e75cfed69637c6d541f3649d24b22
Frank Alimimian <alimimianfrank@gmail.com>
2024-07-18 18:20:24 +0100

    minor update

---
+++ education.js
@@ -0,0 +1,14 @@
+// Education page   
+
+const educationBtn = document.querySelectorAll('.education-btn')
+const education = document.getElementById('education')
+//Education
+
+
+educationBtn.forEach(btn =>{
+    btn.addEventListener("click",(e)=>{
+        e.preventDefault()
+        education.scrollIntoView({behavior:"smooth"})
+    })
+})
+
--- script.js
+++ script.js
@@ -1,18 +1,58 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const toggleButtons = document.querySelectorAll('.toggle-content');
 
-    toggleButtons.forEach(button => {
-        button.addEventListener('click', function() {
-            const course = this.closest('.course');
-            course.classList.toggle('expanded');
-            console.log("Js here");
-            if (course.classList.contains('expanded')) {
-                this.textContent = 'Read Less';
-            } else {
-                this.textContent = 'Read More';
-            }
-        });
-    });
+
+const aboutMeBtn = document.querySelectorAll(".aboutme-btn");
+const skillsBtn = document.querySelectorAll(".skills-btn");
+const projectBtn = document.querySelectorAll(".project-btn");
+const contactsBtn = document.querySelectorAll(".contacts-btn");
+
+const aboutMe = document.getElementById("about-me");
+const skills = document.getElementById("skills-tools");
+const projects = document.getElementById("projects");
+const contacts = document.getElementById("contacts");
+
+//mobile navigation
+const mobileMenu = document.querySelector('.mobile-menu')
+const showMenu = document.getElementsByClassName('show-menu')[0]
+const closeMenu = document.getElementsByClassName('close-menu')[0]
+
+aboutMeBtn.forEach(btn =>{
+    btn.addEventListener("click", (e)=>{
+        e.preventDefault()
+        aboutMe.scrollIntoView({behavior:"smooth"})
+        console.log("js here")
+    })
+})
+
+skillsBtn.forEach(btn =>{
+    btn.addEventListener("click", (e)=>{
+        e.preventDefault()
+        skills.scrollIntoView({behavior:"smooth"})
+        console.log("js here")
+    })
+})
+
+projectBtn.forEach(btn =>{
+    btn.addEventListener("click", (e)=>{
+        e.preventDefault()
+        projects.scrollIntoView({behavior:"smooth"})
+        console.log("js here")
+    })
 });
 
-console.log("Js here");
+contactsBtn.forEach(btn =>{
+    btn.addEventListener("click", (e)=>{
+        e.preventDefault()
+        contacts.scrollIntoView({behavior:"smooth"})
+        console.log("js here")
+    })
+});
+
+
+showMenu.addEventListener('click', (e)=>{
+    
+    mobileMenu.style.display ="flex"
+})
+
+closeMenu.addEventListener('click',(e)=>{
+    mobileMenu.style.display ="none"
+})
